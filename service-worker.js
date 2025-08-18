4b76444c9b4e843d5b04fdd3c2898405961dcb4f
NicoMedula <nicolasmedula@hotmail.com>
2025-08-18 13:44:43 -0300

    Cambio de acentos para correcta lectura dentro del entorno

--- service-worker.js
+++ service-worker.js
@@ -4,11 +4,7 @@ const urlsToCache = [
   "/index.html",
   "/manifest.json",
   "/logo.png",
-  "/models/old_tyre_1k.gltf",
-  "/models/old_tyre.bin",
-  "/models/textures/old_tyre_nor_gl_1k.jpg",
-  "/models/textures/old_tyre_arm_1k.jpg",
-  "/models/textures/old_tyre_diff_1k.jpg",
+  "/models-prueba/Iso.gltf",
   "https://aframe.io/releases/1.4.0/aframe.min.js",
 ];
 
